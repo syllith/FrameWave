45ad04791786c9dd9c1a7feafa411146aa5560ff
syllith <kinlough@digi-safe.co>
2023-09-13 14:54:58 -0500

    Very broken

--- ui/ui.go
+++ ui/ui.go
@@ -31,36 +31,26 @@ import (
 )
 
 // * Backend
+type CameraSettings struct {
+	Name       string
+	Res        string
+	FPS        int
+	Quality    int
+	Port       int
+	Enabled    bool
+	BufferSize int
+	MaxFPS     int
+}
+
 var stream chan []byte
 var server *http.Server
 var stopChan chan bool
 var ffmpegCmd *exec.Cmd
-var bufferSize = 0
 var ffmpegPath = filepath.Join(general.RoamingDir(), "FrameWave", "ffmpeg.exe")
+var cameras []CameraSettings
 
-// * Main view
-var mainView = container.NewPadded(
-	container.NewCenter(
-		container.NewVBox(
-			streamImg,
-			container.New(&fynecustom.MinWidthFormLayout{MinColWidth: 200},
-				&widget.Label{Text: "Camera"},
-				cameraSelect,
-				&widget.Label{Text: "Resolution"},
-				resSelect,
-				fpsLabel,
-				fpsSlider,
-				&widget.Label{Text: "Quality"},
-				qualitySlider,
-				&widget.Label{Text: "Port"},
-				portEntry,
-			),
-			currentBufferLabel,
-			currentFpsLabel,
-			toggleButton,
-		),
-	),
-)
+// * Main viewd
+var mainView = container.NewBorder(nil, toggleButton, nil, nil, getTabs())
 
 // * Elements
 var streamImg = &fynecustom.CustomImage{
@@ -69,114 +59,24 @@ var streamImg = &fynecustom.CustomImage{
 	Image:       &canvas.Image{FillMode: canvas.ImageFillStretch},
 }
 
-var fpsSlider = &widget.Slider{
-	Min:   1,
-	Max:   30,
-	Value: 30,
-}
-
-var fpsLabel = &widget.Label{
-	Text: "FPS",
-}
-
-var currentFpsLabel = &canvas.Text{
-	Text:     "FPS: N/A",
-	Color:    colormap.White,
-	TextSize: 14,
-}
-
-var currentBufferLabel = &canvas.Text{
-	Text:     "Buffer Size: N/A",
-	Color:    colormap.OffWhite,
-	TextSize: 14,
-}
-
-var cameraSelect = &widget.Select{
-	PlaceHolder: "Camera",
-}
-
-var resSelect = &widget.Select{
-	PlaceHolder: "Resolution",
-}
-
-var portEntry = &widget.Entry{
-	PlaceHolder: "Enter Port",
-	Text:        "8080",
-}
-
 var toggleButton = &widget.Button{
 	Text: "Start",
 }
 
-var qualitySlider = &widget.Slider{
-	Min:   0,
-	Max:   100,
-	Value: 100,
+var currentFpsLabel = &canvas.Text{
+	Text:     "FPS: N/A",
+	Color:    colormap.OffWhite,
+	TextSize: 14,
 }
 
 // . Initalization
 func Init() {
-	//. Update FPS label on slider move
-	fpsSlider.OnChanged = func(f float64) {
-		fpsLabel.SetText(fmt.Sprintf("FPS (%v)", int(f)))
-	}
-
-	//. Toggle button tapped
-	toggleButton.OnTapped = func() {
-		if toggleButton.Text == "Start" {
-			startStreaming()
-		} else {
-			stopStreaming()
+	go func() {
+		for {
+			time.Sleep(3 * time.Second)
+			fmt.Println(cameras)
 		}
-		toggleButton.Refresh()
-	}
-
-	//. Set buffer on resolution change
-	resSelect.OnChanged = func(selected string) {
-		re := regexp.MustCompile(`(\d+)x(\d+)`)
-		matches := re.FindStringSubmatch(selected)
-
-		width, _ := strconv.Atoi(matches[1])
-		height, _ := strconv.Atoi(matches[2])
-
-		uncompressedSize := width * height * 24 / 8
-		estimatedJPEGSize := uncompressedSize / 10
-		bufferSize = estimatedJPEGSize + int(0.3*float64(estimatedJPEGSize))
-		currentBufferLabel.Text = fmt.Sprintf("Buffer Size: %s", general.HumanReadableSize(bufferSize))
-		currentBufferLabel.Refresh()
-	}
-
-	//. Get camera friendly names
-	cameraSelect.Options = getCameraNames()
-
-	//. On camera change
-	cameraSelect.OnChanged = func(selected string) {
-		if cameraSelect.SelectedIndex() > -1 {
-			//* Camera found, get resolution
-			resolutions := getCameraResolutions(selected)
-			resSelect.Options = resolutions
-			if len(resolutions) > 0 {
-				//* Set default resolution
-				resSelect.SetSelected(resolutions[0])
-				resSelect.Enable()
-			}
-		} else {
-			//! No cameras found
-			resSelect.Options = []string{}
-			resSelect.Refresh()
-			resSelect.Disable()
-		}
-	}
-
-	//. Select camera
-	if len(cameraSelect.Options) > 0 {
-		//* Camera found, select first one
-		cameraSelect.SetSelected(cameraSelect.Options[0])
-	} else {
-		//! No camera selected, disabled resolution
-		resSelect.Disable()
-	}
-
+	}()
 	//. Create stream buffer
 	stream = make(chan []byte, 100)
 
@@ -235,20 +135,21 @@ func startStreaming() {
 	toggleButton.SetText("Stop")
 	streamImg.Show()
 
-	host := "0.0.0.0:" + portEntry.Text
-	stopChan = make(chan bool)
-	go mjpegCapture(cameraSelect.Selected, host)
+	for _, camera := range cameras {
+		if camera.Enabled {
+			host := "0.0.0.0:" + strconv.Itoa(camera.Port)
+			go mjpegCapture(camera)
 
-	if server == nil {
-		//* Create server
-		server = &http.Server{Addr: host}
-		go func() {
-			//* Listen and serve
-			if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-				//! Failed to start server
-				log.Println("Failed to start server:", err)
+			if server == nil {
+				// Create server
+				server = &http.Server{Addr: host}
+				go func() {
+					if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+						log.Println("Failed to start server:", err)
+					}
+				}()
 			}
-		}()
+		}
 	}
 }
 
@@ -293,20 +194,18 @@ func stopStreaming() {
 }
 
 // . FFMPEG Capture
-func mjpegCapture(deviceName, host string) {
+func mjpegCapture(camera CameraSettings) {
 	//* Configure FFMPEG
 	ffmpegArgs := []string{
 		"-f", "dshow",
-		"-rtbufsize", strconv.Itoa(bufferSize),
-		"-probesize", "32",
-		"-analyzeduration", "0",
-		"-i", "video=" + deviceName,
-		"-pix_fmt", "yuv420p",
-		"-color_range", "2",
-		"-vf", "scale=in_range=pc:out_range=pc,scale=" + resSelect.Selected + fmt.Sprintf(",fps=%v", fpsSlider.Value),
+		"-i", fmt.Sprintf("video=%s", camera.Name),
+		"-vf", fmt.Sprintf("fps=%d", camera.FPS),
+		"-video_size", camera.Res,
+		"-bufsize", fmt.Sprintf("%d", camera.BufferSize),
 		"-c:v", "mjpeg",
-		"-q:v", strconv.Itoa(100 - int(qualitySlider.Value)),
-		"-f", "mjpeg", "-",
+		"-q:v", "5",
+		"-f", "mjpeg",
+		"-",
 	}
 
 	//* Build command
@@ -367,8 +266,8 @@ func mjpegCapture(deviceName, host string) {
 	go func() {
 		//* Create JPEG read buffer
 		jpegEnd := []byte{0xFF, 0xD9}
-		buffer := make([]byte, 0, bufferSize)
-		readBuffer := make([]byte, bufferSize)
+		buffer := make([]byte, 0, camera.BufferSize)
+		readBuffer := make([]byte, camera.BufferSize)
 		for {
 			select {
 			case <-stopChan:
@@ -411,8 +310,8 @@ func mjpegCapture(deviceName, host string) {
 			}
 
 			//* Limit the 'buffer' size by keeping only the most recent data
-			if len(buffer) > bufferSize {
-				buffer = buffer[len(buffer)-bufferSize:]
+			if len(buffer) > camera.BufferSize {
+				buffer = buffer[len(buffer)-camera.BufferSize:]
 			}
 		}
 	}()
@@ -491,10 +390,11 @@ func getCameraResolutions(deviceName string) []string {
 	}
 
 	//. Update max FPS label and slider
-	fpsSlider.Max = float64(maxFps)
-	if maxFps < int(fpsSlider.Value) || fpsLabel.Text == "FPS" {
-		fpsSlider.SetValue(float64(maxFps))
-		fpsLabel.SetText(fmt.Sprintf("FPS (%v)", int(maxFps)))
+	for i, cam := range cameras {
+		if cam.Name == deviceName {
+			cameras[i].MaxFPS = maxFps
+			break
+		}
 	}
 
 	//* Sort resolutions
@@ -514,4 +414,130 @@ func getCameraResolutions(deviceName string) []string {
 	})
 
 	return resolutions
+}
+
+func genCameraContainer(cameraName string) *fyne.Container {
+	//. Create enabled checkbox
+	enabledCheck := &widget.Check{
+		OnChanged: func(checked bool) {
+			for i, cam := range cameras {
+				if cam.Name == cameraName {
+					cameras[i].Enabled = checked
+					break
+				}
+			}
+		},
+	}
+
+	//. Create resolution drop down
+	resSelect := &widget.Select{
+		PlaceHolder: "Resolution",
+		Options:     getCameraResolutions(cameraName),
+		OnChanged: func(selected string) {
+			for i, cam := range cameras {
+				if cam.Name == cameraName {
+					cameras[i].Res = selected
+
+					re := regexp.MustCompile(`(\d+)x(\d+)`)
+					matches := re.FindStringSubmatch(selected)
+
+					width, _ := strconv.Atoi(matches[1])
+					height, _ := strconv.Atoi(matches[2])
+
+					uncompressedSize := width * height * 24 / 8
+					estimatedJPEGSize := uncompressedSize / 10
+					cameras[i].BufferSize = estimatedJPEGSize + int(0.3*float64(estimatedJPEGSize))
+					break
+				}
+			}
+		},
+	}
+
+	//. Create FPS slider and label
+	fpsSlider := &widget.Slider{
+		Min:   1,
+		Max:   30,
+		Value: 30,
+	}
+
+	fpsLabel := &widget.Label{
+		Text: fmt.Sprintf("FPS (%v)", int(fpsSlider.Value)),
+	}
+
+	//. Create quality slider
+	qualitySlider := &widget.Slider{
+		Min:   0,
+		Max:   100,
+		Value: 100,
+		OnChanged: func(f float64) {
+			for i, cam := range cameras {
+				if cam.Name == cameraName {
+					cameras[i].Quality = int(f)
+					break
+				}
+			}
+		},
+	}
+
+	//. Create port entry
+	portEntry := &widget.Entry{
+		PlaceHolder: "Enter Port",
+		Text:        "8080",
+	}
+
+	//. Set default resolutions
+	if resSelect.Options != nil && len(resSelect.Options) > 0 {
+		resSelect.SetSelected(resSelect.Options[0])
+	}
+
+	//. Update FPS label on slider move
+	fpsSlider.OnChanged = func(f float64) {
+		fpsLabel.SetText(fmt.Sprintf("FPS (%v)", int(f)))
+		for i, cam := range cameras {
+			if cam.Name == cameraName {
+				cameras[i].FPS = int(f)
+				break
+			}
+		}
+	}
+
+	//. Toggle button tapped
+	toggleButton.OnTapped = func() {
+		if toggleButton.Text == "Start" {
+			startStreaming()
+		} else {
+			stopStreaming()
+		}
+		toggleButton.Refresh()
+	}
+
+	// Return the VBox containing all these widgets for this camera.
+	return container.NewPadded(
+		container.New(&fynecustom.MinWidthFormLayout{MinColWidth: 200},
+			&widget.Label{Text: "Enabled"},
+			enabledCheck,
+			&widget.Label{Text: "Resolution"},
+			resSelect,
+			fpsLabel,
+			fpsSlider,
+			&widget.Label{Text: "Quality"},
+			qualitySlider,
+			&widget.Label{Text: "Port"},
+			portEntry,
+		),
+	)
+}
+
+// Dynamic function to get the tabs based on available cameras
+func getTabs() *container.AppTabs {
+	tabs := container.NewAppTabs()
+	tabs.SetTabLocation(container.TabLocationLeading)
+	cameraNames := getCameraNames()
+
+	for _, name := range cameraNames {
+		cameras = append(cameras, CameraSettings{Name: name})
+		tabs.Append(container.NewTabItem(name, genCameraContainer(name)))
+	}
+
+	return tabs
 }
